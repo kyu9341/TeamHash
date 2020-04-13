0ed50ba2b4c0b1f39cc1bfd038da2c3b45e6ca65
조승현 <49122299+wjrmffldrhrl@users.noreply.github.com>
2020-04-13 19:42:06 +0900

    jpa test

--- jo/spring_boot/workspace/my-sllipp/src/main/java/net/sllipp/controller/User.java
+++ jo/spring_boot/workspace/my-sllipp/src/main/java/net/sllipp/controller/User.java
@@ -6,34 +6,25 @@ public class User {
     String name;
     String email;
 
-   
-
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public String getUserId() {
+        return userId;
     }
 
-    public String getUserId(){
-        return this.userId;
+
+    public String getPassword() {
+        return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getName() {
+        return name;
     }
 
-  
-    public void setName(String name) {
-        this.name = name;
+    public String getEmail() {
+        return email;
     }
 
-  
     public void setEmail(String email) {
         this.email = email;
     }
-
-    @Override
-    public String toString() {
-        return "User [email=" + email + ", name=" + name + ", password=" + password + ", userId=" + userId + "]";
-    }
-
     
 }
