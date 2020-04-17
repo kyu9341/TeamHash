e55deb4e7d891afb53ba702c3da6b0b356375e3b
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-04-18 00:30:58 +0900

    Accessing data with mariadb and JPA

---
+++ jo/spring_boot/workspace/accessing_data_db/src/main/java/com/springboot/accessing_data_db/AccessingDataMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.accessing_data_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AccessingDataMysqlApplication.class, args);
+  }
+
+}
---
+++ jo/spring_boot/workspace/accessing_data_db/src/main/java/com/springboot/accessing_data_db/UserRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.accessing_data_db;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springboot.accessing_data_db.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
