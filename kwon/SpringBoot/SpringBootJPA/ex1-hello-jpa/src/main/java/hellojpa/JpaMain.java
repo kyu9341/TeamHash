3b7d3ccfa9cda7a4281f00f880b2cdeea0f10f82
kyu9341 <kyu9341@naver.com>
2020-04-18 00:01:04 +0900

    jpa

--- kwon/SpringBoot/SpringBootJPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ kwon/SpringBoot/SpringBootJPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -12,12 +13,16 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Member member = new Member();
+            // Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(2)
+                    .setMaxResults(10)
+                    .getResultList();
 
-            member.setId(2L);
-            member.setName("HelloB");
-            // 저장
-            em.persist(member);
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
             // 커밋
             tx.commit();
         } catch (Exception e){ // 문제가 생긴경우
