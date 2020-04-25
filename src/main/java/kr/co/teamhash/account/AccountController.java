2b8caddd6002de1ce30983cd41aa32dd0f0bc1e2
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-04-25 16:00:55 +0900

    unused import del

--- src/main/java/kr/co/teamhash/account/AccountController.java
+++ src/main/java/kr/co/teamhash/account/AccountController.java
@@ -2,7 +2,6 @@ package kr.co.teamhash.account;
 
 import kr.co.teamhash.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
