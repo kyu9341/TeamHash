0b942366ef727fdc57c805f5ee25557c46be5616
kyu9341 <kyu9341@naver.com>
2020-05-02 10:27:18 +0900

    로그인 실패, 로그아웃 테스트 코드 추가

--- src/test/java/kr/co/teamhash/main/MainControllerTest.java
+++ src/test/java/kr/co/teamhash/main/MainControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,6 +52,29 @@ class MainControllerTest {
                 .andExpect(authenticated().withUsername("teamhash"));
         // teamhash 이란 이름으로 인증이 될 것이다. because UserAccount 에서 username 을 nickname 으로 리턴하였기 때문
 
+    }
+
+    @DisplayName("로그인 실패")
+    @Test
+    void login_fail() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("username","11111111111")
+                .param("password","0000000000")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
+
+    @WithMockUser
+    @DisplayName("로그아웃")
+    @Test
+    void logout() throws Exception {
+        mockMvc.perform(post("/logout")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
     }
 
 }
