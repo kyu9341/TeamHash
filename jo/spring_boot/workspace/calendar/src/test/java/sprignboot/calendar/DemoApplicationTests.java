279f762c5b6b86ab3af05eb2c0b603e62b341bb9
조승현 <49122299+wjrmffldrhrl@users.noreply.github.com>
2020-05-22 18:04:52 +0900

    test calendar

---
+++ jo/spring_boot/workspace/calendar/src/test/java/sprignboot/calendar/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sprignboot.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
